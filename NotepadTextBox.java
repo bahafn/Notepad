40a1b02cfd524083ffef2fb2d8b40cbd4c054190
Baha Al-Khateeb <babaalkhateeb@gmail.com>
2023-07-24 15:38:48 +0300

    Status bar works now.

--- NotepadTextBox.java
+++ NotepadTextBox.java
@@ -5,6 +5,7 @@ import javax.swing.JTextArea;
 
 public class NotepadTextBox extends JTextArea {
     private boolean statusBar;
+    int ln, col; // These are used to sure we need a repaint
 
     public NotepadTextBox(boolean statusBar, Dimension size, String text, boolean lineWrap) {
         this(statusBar, text, lineWrap);
@@ -24,8 +25,26 @@ public class NotepadTextBox extends JTextArea {
         if (!statusBar)
             return;
 
-        g.drawString("Ln: " + getText().substring(0, getSelectionEnd()).split("\n").length + ", Col: " + getSelectionStart(), 10, getHeight() - 5);
-        repaint();
+        // Get array of every line before the selected line
+        String[] lines = getText().substring(0, getSelectionEnd()).split("\r\n|\n|\r", -1);
+
+        // Get the index of the selected character (this index is for all lines not just the selected one)
+        int col = getSelectionEnd() + 1;
+
+        // Remove the characters of all the other lines from the index of the selected character
+        for (int i = 0; i < lines.length - 1; i++)
+            col -= lines[i].length() + 1;
+
+        int ln = lines.length; // The length of the array is the line we are on
+
+        g.drawString("Ln: " + ln + ", Col: " + col, 10, getHeight() - 5); // Draw status bar
+        
+        // Check if anything changed to do a repaint (this saves memory)
+        if (this.ln != ln || this.col != col) {
+            this.ln = ln;
+            this.col = col;
+            repaint();
+        }
     }
 
     public void changeStatusBar() {
