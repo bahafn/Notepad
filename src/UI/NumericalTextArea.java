da1d7a067f65b4fc2dd8d9d55262c1143d2f5d4a
Baha Al-Khateeb <babaalkhateeb@gmail.com>
2023-07-26 16:47:04 +0300

    Added comments to NumericalTextArea.

--- src/UI/NumericalTextArea.java
+++ src/UI/NumericalTextArea.java
@@ -8,15 +8,17 @@ import java.awt.Graphics;
 import java.awt.Dimension;
 
 public class NumericalTextArea extends JTextArea {
-    private String regex;
-    private String removeString = null;
+    private String regex; // Saves the the character that are allowed to be used
+    private String removeString = null; // The string to remove in the next call to paintComponent
 
     public NumericalTextArea(String text, Dimension size, boolean integer, boolean positive) {
+        // Set the regex (allowed characters)
         if (integer)
             regex = positive ? "^[1234567890]+$" : "^[-1234567890]+$";
         else
             regex = positive ? "^[1234567890.]+$" : "^[-1234567890.]+$";
 
+        // Add document listener to keep track of all changes that happen to text
         getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void changedUpdate(DocumentEvent e) { handleUserChange(e.getOffset(), e.getLength()); }
@@ -36,10 +38,13 @@ public class NumericalTextArea extends JTextArea {
             removeString = change;
     }
 
+    // Return true if the keeps the text numerical
+    // TODO: Add all contions to this function like not begin able to put two . or -
     private boolean checkChange(String change) {
         return change.matches(regex);
     }
 
+    // Overrides JTextArea's setText method so you can't use it for non-numerical values
     public void setText(String text) {
         if (!checkChange(text))
             throw new IllegalArgumentException("NumericalTextArea can't have non-numerical string.");
@@ -48,6 +53,7 @@ public class NumericalTextArea extends JTextArea {
     }
 
     public void paintComponent(Graphics g) {
+        // Remove any unwanted string (string that are in removeString)
         if (removeString != null) {
             super.setText(getText().substring(0, getText().indexOf(removeString)));
             removeString = null;
