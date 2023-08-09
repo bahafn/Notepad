6bf1f5d7577ae6b45220c5524ee385505e25e81e
Baha Al-Khateeb <babaalkhateeb@gmail.com>
2023-08-09 18:48:47 +0300

    FontWindow's all options work now (I think).

--- src/WindowClasses/FontWindow.java
+++ src/WindowClasses/FontWindow.java
@@ -2,7 +2,9 @@ package WindowClasses;
 
 import java.awt.GraphicsEnvironment;
 import java.awt.GridLayout;
+import java.awt.Font;
 
+import javax.swing.JComboBox;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
@@ -22,17 +24,24 @@ public class FontWindow extends MemorySafeWindow {
     }
 
     private void showGUI() {
-        setLayout(new GridLayout(2, 2));
+        setLayout(new GridLayout(3, 2));
 
         add(UICreator.createJLabel("Font:", UICreator.DEFAULT_TEXT_SIZE, 2, UICreator.DEFAULT_FONT));
-        add(UICreator.createJComboBox(
+
+        // Create change font option
+        JComboBox<String> fontFamilyBox = UICreator.createJComboBox(
                 GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames(),
-                app.getFont().getFamily()));
+                app.getFont().getFamily());
+
+        fontFamilyBox.addActionListener(e -> changeFont((String) fontFamilyBox.getSelectedItem()));
+        add(fontFamilyBox);
 
         add(UICreator.createJLabel("Size:", UICreator.DEFAULT_TEXT_SIZE, 2, UICreator.DEFAULT_FONT));
 
+        // Create change size option
         NumericalTextArea textArea = UICreator.createNumericalTextArea(String.valueOf(app.getFont().getSize()),
                 UICreator.DEFAULT_SIZE, false, true, false);
+        // Document listener so the user sees the changes the moment he changes them
         textArea.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void changedUpdate(DocumentEvent e) {
@@ -50,9 +59,25 @@ public class FontWindow extends MemorySafeWindow {
             }
         });
         add(textArea);
+
+        add(UICreator.createJLabel("Style:", UICreator.DEFAULT_TEXT_SIZE, 2, UICreator.DEFAULT_FONT));
+
+        // Create change style option
+        JComboBox<String> fontStyleBox = UICreator.createJComboBox(new String[] { "Regular", "Bold", "Italic" },
+                "Regular");
+        fontStyleBox.addActionListener(e -> changeStyle(fontStyleBox.getSelectedIndex()));
+        add(fontStyleBox);
     }
 
     private void changeSize(float size) {
         app.setFont(app.getFont().deriveFont(size));
+    }
+
+    private void changeFont(String fontName) {
+        app.setFont(new Font(fontName, app.getFont().getStyle(), app.getFont().getSize()));
+    }
+
+    private void changeStyle(int style) {
+        app.setFont(new Font(app.getFont().getFamily(), style, app.getFont().getSize()));
     }
 }
